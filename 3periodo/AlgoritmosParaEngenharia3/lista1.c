37146b81a93492a07cde525ccd0dd61350882bb3
Gabriel <59960351+gabrielpagotto@users.noreply.github.com>
2022-02-23 22:34:55 -0300

    Conclui atividade

--- 3periodo/AlgoritmosParaEngenharia3/lista1.c
+++ 3periodo/AlgoritmosParaEngenharia3/lista1.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
+#include <string.h>
+
+int tamanhoPadraoVetor = 0;
 
 // (Exercicio1)
 // Faça uma função que recebe por parâmetro o raio de uma esfera e calcula o seu
@@ -124,7 +127,124 @@ bool eNumeroPositivo(int numero)
 
 char *pegarTipoTriangulo(int x, int y, int z)
 {
-    if (x)
+    if (z > (x + y) || x > (y + z) || y > (x + z))
+    {
+        return "invalido";
+    }
+
+    if (x, y == z && y, z == x && x, z == y)
+    {
+        return "Triangulo Equilatero";
+    }
+    
+    if (x == y || x == z || y == z)
+    {
+        return "Triangulo Isosceles";
+    }
+
+    if (x, y != z && y, z != x && x, z != y)
+    {
+        return "Triangulo Escaleno";
+    }
+
+    return "";
+}
+
+// (Exercicio8)
+// Faça uma função que leia um número não determinado de valores positivos e
+// retorna a média aritmética dos mesmos.
+double pegarMediaDeListaNumeros(int *valores)
+{   
+    int somaValores = 0.00;
+    double media;
+
+    for (int i = 0; i < tamanhoPadraoVetor; i++)
+    {
+        somaValores += valores[i];
+    }
+
+    media = somaValores / tamanhoPadraoVetor;
+    return media;
+}
+
+// (Exercicio9)
+// Escreva uma função que recebe, por parâmetro, dois valores X e Z e calcula e
+// retorna X elevado a Z. (sem utilizar funções ou operadores de potência prontos)
+int elevarNumeros(int x, int z)
+{
+    int resultado = 0;
+    
+    for (int i = 0; i < z; i++)
+    {   
+        if (resultado == 0)
+        {
+            resultado = x;
+        }
+        else {
+            resultado = resultado * x;
+        }
+    }
+
+    return resultado;
+}
+
+// (Exercicio10)
+// Faça uma função que recebe, por parâmetro, um valor inteiro e positivo e retorna o
+// número de divisores desse valor.
+int verificarQuantidadeDivisores(int numero)
+{
+    int quantidade = 0;
+
+    for (int i = 1; i < numero; i++)
+    {
+        if ((numero % i) == 0)
+        {
+            quantidade++;
+        }
+    }
+
+    return quantidade;
+}
+
+// (Exercicio11)
+// Escreva uma função que recebe, por parâmetro, um valor inteiro e positivo e
+// retorna o somatório desse valor.
+int realizarSomatoria(int valor)
+{
+    return valor + valor;
+}
+
+// (Exercicio12)
+// Escreva um procedimento que receba um número inteiro. Esta função deve
+// verificar se tal número é primo. No caso positivo, a função deve retornar 1, caso
+// contrário zero.
+int eNumeroPrimo(int numero)
+{   
+    int resultado = 1;
+
+    for (int i = 2; i <= numero / 2; i++) {
+        if (numero % i == 0) {
+            resultado = 0;
+            break;
+        }
+    }
+
+    return resultado;
+}
+
+// (Exercicio12)
+// Criar uma função que verifique quantas vezes um número inteiro x é divisível por
+// um número inteiro y. A função deve retornar -1 caso não seja possível calcular.
+int verificarQuantidadeVezesNumeroDivisivel(x, y)
+{
+    int resultado = -1;
+
+    if ((x % y) == 0)
+    {
+        return x / y;
+    }
+
+    return resultado;
 }
 
 void rodarExercicio1()
@@ -198,6 +318,7 @@ void rodarExercicio6()
     printf("Informe um numero: ");
     scanf("%d", &numero);
     ePositivo = eNumeroPositivo(numero);
+
     if (ePositivo)
     {
         printf("%d e positivo!\n", numero);
@@ -210,17 +331,137 @@ void rodarExercicio6()
 
 void rodarExercicio7()
 {
-    printf("\nEXERCICIO 6\n");
+    printf("\nEXERCICIO 7\n");
+    char *resultado;
+    int x, y, z;
+    printf("Informe os lados do triangulo (x y z)");
+    scanf("%d%d%d", &x, &y, &z);
+    resultado = pegarTipoTriangulo(x, y, z);
+
+    if (strcmp(resultado, "invalido") == 0)
+    {
+        printf("Os valores informados nao formam um triangulo.\n");
+    }
+    else if (strcmp(resultado, "") == 0)
+    {
+        printf("Dados informados invalidos,\n");
+    }
+    else
+    {
+        printf("O triangulo de lados x=%d, y=%d e z=%d e %s,\n", x, y, z, resultado);
+    }
+}
+
+void rodarExercicio8()
+{
+    printf("\nEXERCICIO 7\n");
+    int *valores;
+    valores = (int*) malloc(0);
+    char rodar = 's';
+    double media;
+    int i = 0;
+
+    while (rodar == 's')
+    {
+        printf("Informe um numero: ");
+        scanf("%d", &valores[i]);
+        tamanhoPadraoVetor++;
+        realloc(valores, tamanhoPadraoVetor);
+        i++;
+        printf("Adicionar outro? s/n ");
+        scanf("%s", &rodar);
+    }
+
+    media = pegarMediaDeListaNumeros(valores);
+    printf("A media aritimetica dos valores e %lf\n", media);
+}
+
+void rodarExercicio9()
+{
+    printf("\nEXERCICIO 9\n");
+    int x, z, resultado;
+    printf("Informe o valores a serem elevados (x z): ");
+    scanf("%d%d", &x, &z);
+    resultado = elevarNumeros(x, z);
+    printf("%d^%d = %d\n", x, z, resultado);
+}
+
+void rodarExercicio10()
+{
+    printf("\nEXERCICIO 10\n");
+    int numero, divisores;
+    printf("Informe um numero: ");
+    scanf("%d", &numero);
+    divisores = verificarQuantidadeDivisores(numero);
+    printf("O numero %d possuir %d divisores", numero, divisores);
+}
+
+void rodarExercicio11()
+{
+    printf("\nEXERCICIO 11\n");
+    int numero, resultado;
+    printf("Informe um numero inteiro e positivo: ");
+    scanf("%d", &numero);
+    if (numero <= 0)
+    {
+        printf("Numero informado nao e positivo.\n");
+    }
+    else
+    {
+        resultado = realizarSomatoria(numero);
+        printf("A somatoria do valor %d e %d\n", numero, resultado);
+    }
+}
+
+void rodarExercicio12()
+{
+    printf("\nEXERCICIO 12\n");
+    int numero, resultado;
+    printf("Informe um numero inteiro e positivo: ");
+    scanf("%d", &numero);
+    resultado = eNumeroPrimo(numero);
+    if (resultado == 1)
+    {
+        printf("E um numero primo\n");
+    }
+    else
+    {
+        printf("Numero nao e primo\n");
+    }
+}
+
+void rodarExercicio13()
+{
+    printf("\nEXERCICIO 13\n");
+    int x, y, resultado;
+    printf("Informe dois numeros inteiros (x y): ");
+    scanf("%d%d", &x, &y);
+    resultado = verificarQuantidadeVezesNumeroDivisivel(x, y);
+    if (resultado == -1)
+    {
+        printf("Numero %d nao e divisivel por %d.\n", x, y);
+    }
+    else 
+    {
+        printf("%d pode ser dividido em %d vezes.\n", x, resultado);
+    }
 }
 
 int main()
 {   
-    // rodarExercicio1();
-    // rodarExercicio2();
-    // rodarExercicio3();
-    // rodarExercicio4();
-    // rodarExercicio5();
-    // rodarExercicio6();
+    rodarExercicio1();
+    rodarExercicio2();
+    rodarExercicio3();
+    rodarExercicio4();
+    rodarExercicio5();
+    rodarExercicio6();
+    rodarExercicio7();
+    rodarExercicio8();
+    rodarExercicio9();
+    rodarExercicio10();
+    rodarExercicio11();
+    rodarExercicio12();
+    rodarExercicio13();
 
     return 0;
 }
